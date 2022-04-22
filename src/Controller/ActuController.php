c5884689ab932d7360db00ecf97ff80522c7dbd1
Pierre <pierre.amboise@yahoo.fr>
2022-04-22 17:21:31 +0200

    feat(actu):Add edit

--- src/Controller/ActuController.php
+++ src/Controller/ActuController.php
@@ -19,6 +19,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class ActuController extends AbstractController
 {
+    public const NUMBER_ITEMS = 9;
+    public const NUMBER_ITEMS_TEXT = 6;
+
 	private $file_uploader;
 
 	public function __construct(FileUploader $file_uploader)
@@ -52,99 +55,8 @@ class ActuController extends AbstractController
 
 		if ($form->isSubmitted() && $form->isValid()){
 
-			// Initialise
-			$ordre = [];
-			$datas = $form->getData();
-
-			// Boucle sur les textes
-			for ($i = 1; $i <= 6; $i++){
-
-				// Text
-				$text = 'getText'.$i;
-				if (null != $datas->$text()){
-
-					// Ordre text
-					$ordre[$request->request->get('ordre_t'.$i)] = 't'.$i;
-
-					// TextClass
-					$textClass = '';
-
-					// Gras
-					if (null !== $request->request->get('gras_'.$i) && $request->request->get('gras_'.$i) == 'on'){
-						$textClass .= 'gras ';
-					}
-
-					// Italique
-					if (null !== $request->request->get('italique_'.$i) && $request->request->get('italique_'.$i) == 'on'){
-						$textClass .= 'italique ';
-					}
-
-					// Entoure
-					if (null !== $request->request->get('entoure_'.$i) && $request->request->get('entoure_'.$i) == 'on'){
-						$textClass .= 'entoure ';
-					}
-
-					// Gros
-					if (null !== $request->request->get('gros_'.$i) && $request->request->get('gros_'.$i) == 'on'){
-						$textClass .= 'taille25 ';
-					}
-
-					// Couleur
-					if ('Noir' != $request->request->get('couleur_'.$i)){
-						$textClass .= $request->request->get('couleur_'.$i).' ';
-					}
-
-					// Retire l'espace de fin
-					if (!empty($textClass)){ $textClass = substr($textClass, 0, -1); }
-
-					// Save textClass
-					$text = 'setText'.$i.'Class';
-					$actu->$text($textClass);
-				}
-			}
-
-			// Boucle sur les photos
-			for ($i = 1; $i <= 3; $i++){
-
-				// Photo
-				if (null != $form['photo'.$i]->getData()){
-
-					$file = $form['photo'.$i]->getData();
-					if ($file){
-						$file_name = $this->file_uploader->upload($file);
-						if (null !== $file_name){
-							// $directory = $this->file_uploader->getTargetDirectory();
-							// $full_path = $directory.'/'.$file_name;
-
-							$text = 'setPhoto'.$i;
-							$actu->$text($file_name);
-
-							// Ordre photo
-							$ordre[$request->request->get('ordre_p'.$i)] = 'p'.$i;
-
-						} else {
-							$error = true;
-							$this->addFlash('error', "Erreur d'upload de l'image ".$i.".");
-						}
-					}
-				} else {
-					$text = 'setPhoto'.$i;
-					$actu->$text(null);
-					$text = 'setPhoto'.$i.'Alt';
-					$actu->$text(null);
-				}
-			}
-
-			// Ordre
-			ksort($ordre);
-			$ordre_text = '';
-			foreach ($ordre as $value){
-				$ordre_text .= $value.'_';
-			}
-
-			// Retire l'espace de fin
-			if (!empty($ordre_text)){ $ordre_text = substr($ordre_text, 0, -1); }
-			$actu->setOrdre($ordre_text);
+			// SetForm
+			$actu = $this->setForm($actu, $form->getData(), $request, $form);
 
 			// Date + auteur
 			$actu
@@ -152,15 +64,20 @@ class ActuController extends AbstractController
 				->setDate(new \Datetime('now'))
 			;
 
-			if (!isset($error)){
+			if ($actu !== false){
 				$actuRepository->add($actu);
 				return $this->redirectToRoute('actu_show', ['id' => $actu->getId()], Response::HTTP_SEE_OTHER);
 			}
 		}
 
+		// Méta-datas
+		$metaDatas['ordre'] = $this->getMetaDatas_ordre($actu->getOrdre());
+		$metaDatas['class'] = $this->getMetaDatas_class($actu);
+
 		return $this->renderForm('actu/add.html.twig', [
 			'actu' => $actu,
 			'form' => $form,
+			'metaDatas' => $metaDatas,
 		]);
 	}
 
@@ -185,13 +102,24 @@ class ActuController extends AbstractController
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()){
-			// $actuRepository->add($actu);
-			return $this->redirectToRoute('actu_actu', [], Response::HTTP_SEE_OTHER);
+
+			// SetForm
+			$actu = $this->setForm($actu, $form->getData(), $request, $form);
+
+			if ($actu !== false){
+				$actuRepository->add($actu);
+				return $this->redirectToRoute('actu_show', ['id' => $actu->getId()], Response::HTTP_SEE_OTHER);
+			}
 		}
+
+		// Méta-datas
+		$metaDatas['ordre'] = $this->getMetaDatas_ordre($actu->getOrdre());
+		$metaDatas['class'] = $this->getMetaDatas_class($actu);
 
 		return $this->renderForm('actu/edit.html.twig', [
 			'actu' => $actu,
 			'form' => $form,
+			'metaDatas' => $metaDatas,
 		]);
 	}
 
@@ -218,5 +146,187 @@ class ActuController extends AbstractController
 		$actuRepository->add($actu);
 
 		return $this->redirectToRoute('actu_show', ['id' => $actu->getId()], Response::HTTP_SEE_OTHER);
+	}
+
+	/**
+	 * @IsGranted("ROLE_ADMIN")
+	 * Traite le formulaire et ses méta-datas
+	 */
+	public function setForm($actu, $datas, $request, $form)
+	{
+		// Initialise
+		$ordre = [];
+
+		// Boucle sur les textes
+		for ($i = 1; $i <= 6; $i++){
+
+			// Text
+			$text = 'getText'.$i;
+			if (null != $datas->$text()){
+
+				// Ordre text
+				$ordre[$request->request->get('ordre_t'.$i)] = 't'.$i;
+
+				// TextClass
+				$textClass = '';
+
+				// Gras
+				if (null !== $request->request->get('gras_'.$i) && $request->request->get('gras_'.$i) == 'on'){
+					$textClass .= 'gras ';
+				}
+
+				// Italique
+				if (null !== $request->request->get('italique_'.$i) && $request->request->get('italique_'.$i) == 'on'){
+					$textClass .= 'italique ';
+				}
+
+				// Entoure
+				if (null !== $request->request->get('entoure_'.$i) && $request->request->get('entoure_'.$i) == 'on'){
+					$textClass .= 'entoure ';
+				}
+
+				// Gros
+				if (null !== $request->request->get('gros_'.$i) && $request->request->get('gros_'.$i) == 'on'){
+					$textClass .= 'taille25 ';
+				}
+
+				// Couleur
+				if ('Noir' != $request->request->get('couleur_'.$i)){
+					$textClass .= $request->request->get('couleur_'.$i).' ';
+				}
+
+				// Retire l'espace de fin
+				if (!empty($textClass)){ $textClass = substr($textClass, 0, -1); }
+
+				// Save textClass
+				$text = 'setText'.$i.'Class';
+				$actu->$text($textClass);
+			}
+		}
+
+		// Boucle sur les photos
+		for ($i = 1; $i <= 3; $i++){
+
+			// Garder intact une photo si keep on
+			if (null !== $request->request->get('photo_keep_'.$i)){
+
+				// Ordre photo
+				$ordre[$request->request->get('ordre_p'.$i)] = 'p'.$i;
+
+			// Si nouvelle photo
+			} elseif (null != $form['photo'.$i]->getData()){
+
+				$file = $form['photo'.$i]->getData();
+				if ($file){
+					$file_name = $this->file_uploader->upload($file);
+					if (null !== $file_name){
+						// $directory = $this->file_uploader->getTargetDirectory();
+						// $full_path = $directory.'/'.$file_name;
+
+						$text = 'setPhoto'.$i;
+						$actu->$text($file_name);
+
+						// Ordre photo
+						$ordre[$request->request->get('ordre_p'.$i)] = 'p'.$i;
+
+					} else {
+						$this->addFlash('error', "Erreur d'upload de l'image ".$i.".");
+						return false;
+					}
+				}
+
+			// Delete photo
+			} else {
+				$text = 'setPhoto'.$i;
+				$actu->$text(null);
+				$text = 'setPhoto'.$i.'Alt';
+				$actu->$text(null);
+			}
+		}
+
+		// Ordre
+		ksort($ordre);
+		$ordre_text = '';
+		foreach ($ordre as $value){
+			$ordre_text .= $value.'_';
+		}
+
+		// Retire l'espace de fin
+		if (!empty($ordre_text)){ $ordre_text = substr($ordre_text, 0, -1); }
+		$actu->setOrdre($ordre_text);
+
+		return $actu;
+	}
+
+	/**
+	 * @IsGranted("ROLE_ADMIN")
+	 * Récupère la liste des éléments à afficher dans l'ordre
+	 */
+	public function getMetaDatas_ordre($ordre){
+
+		$metaDatas_ordre = [];
+		$ordre_array = !empty($ordre)
+			? explode('_', $ordre)
+			: []
+		;
+
+		// Boucle sur les 9 éléments
+		for ($i = 1; $i <= self::NUMBER_ITEMS; $i++){
+
+			// Textes/Images existants
+			if (array_key_exists($i - 1, $ordre_array)){
+				$metaDatas_ordre[$ordre_array[$i - 1]] = $i;
+
+			// Textes/Images par défaut
+			} else {
+
+				$img = true;
+
+				// TEXT
+				for ($ii = 1; $ii <= self::NUMBER_ITEMS_TEXT; $ii++){
+					if (!array_key_exists('t'.$ii, $metaDatas_ordre)){
+						$metaDatas_ordre['t'.$ii] = $i;
+						$img = false;
+						break;
+					}
+				}
+
+				// IMG
+				if ($img){
+					$end_img = self::NUMBER_ITEMS - self::NUMBER_ITEMS_TEXT;
+					for ($ii = 1; $ii <= $end_img; $ii++){
+						if (!array_key_exists('p'.$ii, $metaDatas_ordre)){
+							$metaDatas_ordre['p'.$ii] = $i;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		return $metaDatas_ordre;
+	}
+
+	/**
+	 * @IsGranted("ROLE_ADMIN")
+	 * Récupère la liste des class des éléments à afficher
+	 */
+	public function getMetaDatas_class($actu){
+
+		$metaDatas_class = [];
+
+		// Boucle sur les éléments text
+		for ($i = 1; $i <= self::NUMBER_ITEMS_TEXT; $i++){
+
+			$func = 'getText'.$i.'Class';
+			$class_array = !empty($actu->$func())
+				? explode(' ', $actu->$func())
+				: []
+			;
+
+			$metaDatas_class[$i] = $class_array;
+		}
+
+		return $metaDatas_class;
 	}
 }
