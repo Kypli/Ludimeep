f515f97ac3e60d3a4c4ced2dc0ac27b74d966996
Pierre <pierre.amboise@yahoo.fr>
2023-05-30 16:54:51 +0200

    feat(statuts):ajout statuts

--- src/Controller/Asso/StatutsController.php
+++ src/Controller/Asso/StatutsController.php
@@ -2,18 +2,61 @@
 
 namespace App\Controller\Asso;
 
+use Symfony\Component\Finder\Finder;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/statuts", name="statuts")
+ */
 class StatutsController extends AbstractController
-{
+{	
+	public function __construct($statuts_files)
+	{
+		$this->statuts_files = $statuts_files;
+	}
+
 	/**
-	 * @Route("/statuts", name="statuts")
+	 * @Route("/", name="")
 	 */
 	public function index(Request $request)
 	{
+		$files = [];
+		$finder = new Finder();
+		$finder->files()->in($this->statuts_files);
 
-		return $this->render('asso/statuts/index.html.twig');
+		foreach ($finder as $file){
+
+			if (
+				$file != '..' &&
+				$file !='.' &&
+				$file != '' &&
+				$file != '.htaccess' &&
+				$file!='.gitignore' &&
+				$file!='.hgignore'
+			){
+				$files[] = $file;
+			}
+		}
+
+		return $this->render('asso/statuts/index.html.twig', [
+			'files' => $files,
+		]);
+	}
+
+	/**
+	 * @Route("/export/{fileName}", name="_export")
+	 */
+	public function export($fileName)
+	{
+		// Controle
+		if ($fileName == '' || $fileName == null){
+			dump('Erreur de liens.');
+			die;
+		}
+
+		return $this->file($this->statuts_files.$fileName);
 	}
 }
