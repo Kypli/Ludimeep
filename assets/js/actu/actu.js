8cc42b3a3cc9d702428a6f568ccda8605c877a06
Pierre <pierre.amboise@yahoo.fr>
2024-02-02 13:54:21 +0100

    edit tinymce

--- assets/js/actu/actu.js
+++ assets/js/actu/actu.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
 	// TINYMCE
 	tinymce.init({
 		selector: 'textarea',
+		language_load: true,
+		language_url: 'fr_Fr.js',
+		language: 'fr_FR',
 		plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
 		toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
 		tinycomments_mode: 'embedded',
